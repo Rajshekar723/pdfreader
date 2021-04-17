73d927b831f2f96fa71a97294977aba25257af7a
raj723 <77148175+raj723@users.noreply.github.com>
2021-04-17 16:41:25 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print(23)
+import pyttsx3
+
+import PyPDF2
+book = open('chem.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+speaker = pyttsx3.init()
+page = pdfReader.getPage(7)
+text = page.extractText()
+speaker.say(text)
+speaker.runAndWait()
